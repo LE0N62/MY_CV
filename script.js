66c14f68da1a6089fc7b69e8e95a0864d191ab3b
David :v <24102016@utfv.edu.mx>
2025-09-07 18:57:58 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 
 console.log("CV cargado correctamente");
+document.getElementById("downloadBtn").addEventListener("click", () => {
+  const cv = document.getElementById("cv");
+
+  const opt = {
+    margin: 0,
+    filename: "CV_Luis_David_Camarillo_Leon.pdf",
+    image: { type: "jpeg", quality: 1 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    pagebreak: { mode: ["avoid-all", "css", "legacy"] }
+  };
+
+  html2pdf().set(opt).from(cv).save();
+});
